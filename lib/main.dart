310ddf8299543d2354dcec562755600de2bdf55d
inverse9 <permanawahyu97@gmail.com>
2024-03-06 19:31:01 +0800

    added HomePage class

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,21 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: "FLutter Layout Praktikum",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
