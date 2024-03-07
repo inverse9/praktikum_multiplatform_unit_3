0fcab48535ddd7aeb25713a3a2b4ebfc71fafd49
inverse9 <permanawahyu97@gmail.com>
2024-03-07 14:31:35 +0800

    profile user added

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,82 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Praktikum Layout Flutter"),
+        title: const Text("Unit 2: Profil pengguna"),
+        backgroundColor: const Color.fromARGB(255, 100, 219, 104),
+        centerTitle: true,
       ),
       body: ListView(
         children: [
-          Container(color: Colors.red, height: 500, width: 100),
-          Container(color: Colors.green, height: 500, width: 50),
-          Container(color: Colors.blue, height: 500, width: 80)
+          Image.network(
+            "https://staticg.sportskeeda.com/editor/2021/07/67350-16257407612574-800.jpg",
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(
+                Icons.copy,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.share,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.info,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              ListTile(
+                title: Text("Nama"),
+                subtitle: Text("Bob"),
+              ),
+              ListTile(
+                title: Text("Umur"),
+                subtitle: Text("30"),
+              ),
+              ListTile(
+                title: Text("Alamat"),
+                subtitle: Text("Panjer no 203"),
+              ),
+              ListTile(
+                title: Text("Hobi"),
+                subtitle: Text("Olahraga"),
+              ),
+              ListTile(
+                title: Text("NIK"),
+                subtitle: Text("572883919922233"),
+              ),
+              ListTile(
+                title: Text("Zodiak"),
+                subtitle: Text("Cancer"),
+              ),
+              ListTile(
+                title: Text("Makanan favorit"),
+                subtitle: Text("Bakso"),
+              ),
+              ListTile(
+                title: Text("Minuman favorit"),
+                subtitle: Text("Bir"),
+              ),
+              ListTile(
+                title: Text("Cita cita"),
+                subtitle: Text("Pilot"),
+              ),
+              ListTile(
+                title: Text("Pekerjaan"),
+                subtitle: Text("mahasiswa"),
+              ),
+            ],
+          )
         ],
       ),
     );
